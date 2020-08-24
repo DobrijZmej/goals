3e96826aac0813815d1d4df45aa4a2ee4b3be987
DobrijZmej <DobrijZmej@gmail.com>
2020-08-24 13:25:36 +0300

    Базовый класс цели, и связанные сущности

---
+++ database/migrations/2020_08_24_132126_create_goals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->number("user_id")->unsigned();
+            $table->string("name", 100);
+            $table->text("description");
+            $table->string("currency", 10);
+            $table->decimal("amount_target", 30, 2);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
